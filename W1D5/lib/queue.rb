d77a37224ee17a174c18c76cf0ff651ed55f4b98
Steven Inouye <steveninouye@msn.com>
2018-10-25 21:33:43 -0700

    Add Queue Class

---
+++ W1D5/lib/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def peek
+    @queue.last
+  end
+end
