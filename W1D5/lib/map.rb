2ccf079e38e2dc51ecead54b1d2506ddff3198cf
Steven Inouye <steveninouye@msn.com>
2018-10-25 21:54:22 -0700

    Add #set, #get, #delete, #show, #deep_dup method on Map Class

--- W1D5/lib/map.rb
+++ W1D5/lib/map.rb
@@ -4,18 +4,36 @@ class Map
   end
 
   def set(key,value)
-    
+    value = value.is_a? Array ? Map.deep_dup(value) : value
+    @store << [key, value] if @store.none? {|e| e[0] == key}
   end
 
   def get(key)
-
+    key_val = @store.select {|kv| kv[0] == key}
+    key_val[1]
   end
 
   def delete(key)
-
+    val = get(key)
+    @store.delete_if {|kv| kv[0] == key}
+    [key,val]
   end
 
   def show
+    Map.deep_dup(@store)
+  end
 
+  private
+  self.deep_dup(arr)
+    return arr.dup if arr.none? {|e| e.is_a? Array}
+    result = []
+    arr.each do |el|
+      if el.is_a? Array
+        result << Map.deep_dup(el)
+      else
+        result << el
+      end
+    end
+    result
   end
 end
