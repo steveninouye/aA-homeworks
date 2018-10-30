d5473adcd1b140e6b6d266e41a770bbf58b35790
Steven Inouye <steveninouye@msn.com>
2018-10-29 20:58:56 -0700

    Complete #next_turn and #make_move methods

--- W2D2/lib/board.rb
+++ W2D2/lib/board.rb
@@ -3,6 +3,8 @@ class Board
 
   def initialize(name1, name2)
     @cups = Array.new(14){ |i| i == 6 || i == 13 ? [] : [:stone,:stone,:stone,:stone] }
+    @name1 = name1
+    @name2 = name2
   end
 
   def place_stones
@@ -15,10 +17,36 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      # ended on store -- get to choose where to start again
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # ended on empty cup -- switches players' turns
+      :switch
+    else
+      # ended on cup with stones in it -- automatically starts there
+      ending_cup_idx
+    end
   end
 
   def render
