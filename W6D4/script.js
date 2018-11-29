cb06b0badc1450451cbceb7fb4d11ea1d41f08c6
Steven Inouye <steveninouye@msn.com>
2018-11-28 19:44:36 -0800

    Complete Week 6 Day 4 project

--- W6D4/script.js
+++ W6D4/script.js
@@ -1,31 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // toggling restaurants
+document.addEventListener('DOMContentLoaded', () => {
+   // toggling restaurants
 
-  const toggleLi = (e) => {
-    const li = e.target;
-    if (li.className === "visited") {
-      li.className = "";
-    } else {
-      li.className = "visited";
-    }
-  };
+   const toggleLi = (e) => {
+      const li = e.target;
+      if (li.className === 'visited') {
+         li.className = '';
+      } else {
+         li.className = 'visited';
+      }
+   };
 
-  document.querySelectorAll("#restaurants li").forEach((li) => {
-    li.addEventListener("click", toggleLi);
-  });
+   document.querySelectorAll('#restaurants li').forEach((li) => {
+      li.addEventListener('click', toggleLi);
+   });
 
+   // adding SF places as list items
+   const places = document.getElementById('sf-places');
+   const favForm = document.getElementById('fav-place-form');
+   favForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let inputBox = e.target[0];
+      let li = document.createElement('li');
+      li.textContent = inputBox.value;
+      places.appendChild(li);
+      inputBox.value = '';
+   });
+   // --- your code here!
+   // adding new photos
+   const dogPhotoList = document.getElementById('dog-photos-list');
+   const btn = document.getElementById('add-new-dog-photo');
+   btn.addEventListener('click', () => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+         .then((res) => res.json())
+         .then((data) => {
+            let li = document.createElement('li');
+            let img = document.createElement('img');
+            img.setAttribute('src', data.message);
+            li.appendChild(img);
+            dogPhotoList.appendChild(li);
+         });
+   });
 
-
-  // adding SF places as list items
-
-  // --- your code here!
-
-
-
-  // adding new photos
-
-  // --- your code here!
-
-
-
+   // --- your code here!
 });
